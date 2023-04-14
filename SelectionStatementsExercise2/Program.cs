5a07d9afeca3a788ac2eca935689aee40bc380aa
Hamisi Ngao <newhamisi@gmail.com>
2023-04-13 22:22:54 -0400

    complete ex 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what's your favorite subject?");
+            var favoritSubject = Console.ReadLine();
+
+            switch(favoritSubject){
+                case "Mathematics":
+                    Console.WriteLine("I see you love Mathematics");
+                    break;
+                case "Biology":
+                    Console.WriteLine("Biology is your favorite subject");
+                    break;
+                case "Physics":
+                    Console.WriteLine("It seems you really love physics");
+                    break;
+                case "Chemistry":
+                    Console.WriteLine("You favorite subject is chemistry");
+                    break;
+                case "Arts":
+                    Console.WriteLine("You chose Arts as your favorite subject.");
+                    break;
+                default:
+                    Console.WriteLine("Seems your favorite subject is not in our list. We'll definitely add it.");
+                    break;
+            }
+
+
         }
     }
 }
